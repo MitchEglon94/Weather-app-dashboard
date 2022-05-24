e02500d750f669db0ecd58f09bc660383272a2df
Mitchell Eglon <mitchelleglon@Mitchells-MacBook-Pro-24.local>
2022-05-24 20:42:18 +0200

    second commit changed to weather app

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,68 @@
-// Copyright 2022 mitchelleglon
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// DATE AND TIME DISPLAY
+const dateTime = document.getElementById("date");
+function tick() {
+  const newDate = new Date();
+  dateTime.textContent = `${newDate.toLocaleDateString()} ${newDate.toLocaleTimeString()}`;
+}
+tick();
+setInterval(tick, 1000);
+
+// OBTAIN GEOLOCATION
+
+let lat, lon;
+
+async function getPosition() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition((position, error) => {
+      console.log(position);
+      resolve(position);
+      reject(error);
+    });
+  });
+}
+
+try {
+  const position = await getPosition();
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+} catch (err) {
+  console.log(err);
+}
+
+const API_key = `28a7ee7e99b07e3916c770e8a6e351e4
+`;
+const reverseGeoLookup = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${API_key}`;
+const weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+const dailyEndpoint = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+
+const location = document.getElementById("location");
+const temperature = document.getElementById("temp");
+const description = document.getElementById("description");
+const minMax = document.getElementById("min-max");
+const hourly = document.getElementById("hourly");
+
+try {
+  const response = await fetch(weatherEndpoint);
+  if (!response.ok) throw response;
+  const data = await response.json();
+  console.log(data);
+  renderData(data);
+} catch (err) {
+  console.log(err);
+}
+
+function renderData(data) {
+  const {
+    name,
+    main: { temp, temp_max, temp_min, feels_like, humidity, pressure },
+    weather: {
+      0: { main },
+    },
+  } = data;
+  location.textContent = name;
+  temperature.textContent = `${Math.round(temp)}C`;
+  description.textContent = main;
+  minMax.innerHTML = `<span>L:${Math.round(
+    temp_min
+  )}</span><span> H:${Math.round(temp_max)}</span>`;
+}
